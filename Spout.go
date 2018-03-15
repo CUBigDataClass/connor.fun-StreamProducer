e3df04bc8f0654703b1d2a34b5dcfd43c2ddfa55
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-03-14 23:03:06 -0600

    Made a script that opens a stream of realtime tweets from NYC

---
+++ Spout.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"io/ioutil"
+)
+
+func main() {
+
+	data, err := ioutil.ReadFile("./keys/twitter.json")
+	if err != nil {
+		panic(err)
+	}
+
+	auth := make(map[string]string)
+
+	if err := json.Unmarshal(data, &auth); err != nil {
+		panic(err)
+	}
+
+	config := oauth1.NewConfig(auth["consumerKey"], auth["consumerSecret"])
+	token := oauth1.NewToken(auth["accessToken"], auth["accessSecret"])
+
+	// http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	params := &twitter.StreamFilterParams{
+		Locations:     []string{"-74,40,-73,41"},
+		StallWarnings: twitter.Bool(true),
+	}
+	stream, err := client.Streams.Filter(params)
+
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+    fmt.Printf("%s: %s\n", tweet.User.Name, tweet.Text)
+    fmt.Println(tweet.IDStr)
+	}
+
+	demux.HandleChan(stream.Messages)
+
+}
