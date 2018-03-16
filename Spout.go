f52b1c682a9b90ee651ca0f7d77f19ad8f55e132
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-03-15 22:51:09 -0600

    Updated script to output raw text into Kinesis stream

--- Spout.go
+++ Spout.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"io/ioutil"
@@ -10,19 +12,19 @@ import (
 
 func main() {
 
-	data, err := ioutil.ReadFile("./keys/twitter.json")
+	twit, err := ioutil.ReadFile("./keys/twitter.json")
 	if err != nil {
 		panic(err)
 	}
 
-	auth := make(map[string]string)
+	twitterAuth := make(map[string]string)
 
-	if err := json.Unmarshal(data, &auth); err != nil {
-    panic(err)
+	if err := json.Unmarshal(twit, &twitterAuth); err != nil {
+		panic(err)
 	}
 
-	config := oauth1.NewConfig(auth["consumerKey"], auth["consumerSecret"])
-	token := oauth1.NewToken(auth["accessToken"], auth["accessSecret"])
+	config := oauth1.NewConfig(twitterAuth["consumerKey"], twitterAuth["consumerSecret"])
+	token := oauth1.NewToken(twitterAuth["accessToken"], twitterAuth["accessSecret"])
 
 	// http.Client will automatically authorize Requests
 	httpClient := config.Client(oauth1.NoContext, token)
@@ -34,15 +36,30 @@ func main() {
 		Locations:     []string{"-74,40,-73,41"},
 		StallWarnings: twitter.Bool(true),
 	}
+
+	sess := session.Must(session.NewSession()) // MUST BE RUN IN EC2 or WITH LOCAL 'exports' setup
+	svc := kinesis.New(sess)
+
 	stream, err := client.Streams.Filter(params)
-  if(err != nil) {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
+
+	partitionKey := "1"
+	streamName := "raw-tweets"
+	streamInput := kinesis.PutRecordInput{Data: []byte(""), PartitionKey: &partitionKey, StreamName: &streamName}
 
 	demux := twitter.NewSwitchDemux()
 	demux.Tweet = func(tweet *twitter.Tweet) {
 		fmt.Printf("%s: %s\n", tweet.User.Name, tweet.Text)
 		fmt.Println(tweet.IDStr)
+
+		streamInput.Data = []byte(tweet.Text)
+		responce, err := svc.PutRecord(&streamInput)
+		fmt.Println(responce)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	demux.HandleChan(stream.Messages)
